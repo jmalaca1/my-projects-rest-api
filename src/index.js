824cd2cf84bc1635e075cd32d5842ffb733a3d0e
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-04-12 22:26:51 -0500

    added out to api endpoint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import { Hono } from 'hono'
+import auth from './routes/auth.js'
 import projects from './routes/projects.js'
 import tasks from './routes/tasks.js'
+import { authenticate } from './middleware/authenticate.js'
 import { isApiError } from './utils/errors.js'
 import { sendError } from './utils/response.js'
 
 const app = new Hono()
 const api = new Hono()
 
+
 app.use('*', async (c, next) => {
   c.set('traceId', crypto.randomUUID())
   await next()
 })
 
+api.route('/auth', auth)
+
+api.use('*', authenticate)
 api.route('/projects', projects)
 api.route('/tasks', tasks)
 app.route('/api', api)
