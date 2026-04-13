9d30cc549a106a8d862089b7ff14a2e2706c36fa
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-04-12 21:08:39 -0500

    added auth utils

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,6 @@
 export const TASK_STATUSES = ['todo', 'in_progress', 'done']
+
 export const BCRYPT_ROUNDS = 10
+
+export const ACCESS_TOKEN_TTL_SECONDS = 15 * 60 // 15 minutes
+export const REFRESH_TOKEN_TTL_SECONDS = 7 * 24 * 60 * 60 // 7 days
